a2ad71a844b3c5d0c49c1277bf8f6faa62bb31a8
harrill1 <hharrill12@gmail.com>
2018-08-24 18:23:12 -0500

    Added base expense project

---
+++ Expenses/Expenses/ExpenseStructure.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseStructure.swift
+//  Expenses
+//
+//  Created by Hayden on 8/24/18.
+//  Copyright © 2018 Harrill, Hayden. All rights reserved.
+//
+
+import Foundation
+
+struct Expense {
+    
+    var title: String
+    var amount: Double
+    var date: Date
+    
+}
